9692fa2c7f0ffdc51110d767c80cce55dc0cfcd1
Paul WOISARD <paulwoisard@gmail.com>
2022-09-28 11:41:33 +0200

    mise à jour changement couleur titre, image background strecht, typo corespondant à la maquette

--- JS/scripts.js
+++ JS/scripts.js
@@ -179,11 +179,24 @@ $('#txt-search').keyup(function(){
 
       $.getJSON('https://api.themoviedb.org/3/search/multi?api_key=' + API_KEY + '&query=' + film + '&language=fr-FR', function(data) {
             var img = 'https://image.tmdb.org/t/p/original' + data['results'][0]['backdrop_path'];
-            console.log(img);
-            $('#pageCentre').css('background','url(' + img + ') no-repeat'); 
-            $('#pageCentre').css('background-size', '100%');
+            $('#pageCentre').css('background','url(' + img + ') no-repeat center center fixed'); 
+            $('#pageCentre').css('-webkit-background-size', 'cover');
+            $('#pageCentre').css('-moz-background-size', 'cover');
+            $('#pageCentre').css('-o-background-size', 'cover');
+            $('#pageCentre').css('background-size', 'cover');
             $('#titreVideoEnCours').html(titrefr);
             $('#descriptionEnCours').html(data['results'][0]['overview']);
+            var imgObj = new Image();
+            imgObj.src = img + '?' + new Date().getTime();
+            imgObj.setAttribute('crossOrigin', '');
+            getImageLightness(imgObj.src,function(brightness){
+                  console.log(brightness);
+                  if (brightness < 127) {
+                        $('.titleClass').css("color", "white");
+                  } else {
+                        $('.titleClass').css("color", "black");
+                  }
+            });
       });
   }
 
@@ -204,3 +217,38 @@ $('#txt-search').keyup(function(){
       $('.form-control').val("");
     });
 
+function getImageLightness(imageSrc,callback) {
+      var img = document.createElement("img");
+      img.src = imageSrc + '?' + new Date().getTime();
+      img.setAttribute('crossOrigin', '');
+      img.style.display = "none";
+      document.body.appendChild(img);
+ 
+      var colorSum = 0;
+  
+      img.onload = function() {
+          // create canvas
+          var canvas = document.createElement("canvas");
+          canvas.width = this.width;
+          canvas.height = this.height;
+  
+          var ctx = canvas.getContext("2d");
+          ctx.drawImage(this,0,0);
+  
+          var imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
+          var data = imageData.data;
+          var r,g,b,avg;
+  
+          for(var x = 0, len = data.length; x < len; x+=4) {
+              r = data[x];
+              g = data[x+1];
+              b = data[x+2];
+  
+              avg = Math.floor((r+g+b)/3);
+              colorSum += avg;
+          }
+  
+          var brightness = Math.floor(colorSum / (this.width*this.height));
+          callback(brightness);
+      }
+  }
