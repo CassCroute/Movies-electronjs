28d9f417a92c6722c1d1c22af5b6e5a79c5a8f94
Kijusu <75220799+MaxenceR26@users.noreply.github.com>
2022-09-29 23:05:11 +0200

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,61 @@
+const path = require('path');
+
+const { app, BrowserWindow, ipcMain } = require('electron')
+const ipc = ipcMain
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 220,
+    height: 220,
+    icon: './IMG/CassCroute.ico',
+    frame: false,
+    autoHideMenuBar: true,
+    titleBarStyle: 'hidden',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  win.loadFile('loading.html')
+  win.setResizable(false)
+
+  setTimeout(() => launchIndex(), 6000)
+
+  function launchIndex() {
+    win.loadFile('index.html')
+    win.setResizable(true)
+    win.setContentSize(1080, 608)
+    win.setMinimumSize(1080, 608)
+    win.center()
+  }
+
+
+  ipc.on('minimizeApp', ()=>{
+    win.minimize()
+  })
+
+  ipc.on('maximizeRestoreApp', ()=>{
+    if (win.isMaximized()) {
+      win.restore()
+    } else {
+      win.maximize()
+    }
+  })
+
+  win.on('maximize', ()=>{
+    win.webContents.send('isMaximized')
+  })
+  
+  win.on('unmaximize', ()=>{
+    win.webContents.send('isRestored')
+  })
+  
+  ipc.on('closeApp', ()=>{
+    win.close()
+  })
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
