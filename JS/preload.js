0737e9f8aa858ffd6c7a24f133ca59d09c4892b9
Paul WOISARD <paulwoisard@gmail.com>
2022-10-18 07:10:04 +0200

    preload.js : const userDataPath -> var userDataPath

--- JS/preload.js
+++ JS/preload.js
@@ -6,7 +6,7 @@ const ipc = ipcRenderer
 const fs = require('fs');
 const path = require('path');
 
-const userDataPath = process.env.APPDATA;
+var userDataPath = process.env.APPDATA;
 
 if (process.platform == 'darwin') {
     userDataPath = process.env.HOME + '/Library/Preferences/'
